284053cd93730c9793c6d16f8e19be6aad20b4d9
Ross Savage <ross.savage@agrista.com>
2021-11-22 14:30:16 +0100

    Fix migration scripts

--- db/migrations/20211005073512_create_evses_table.down.sql
+++ db/migrations/20211005073512_create_evses_table.down.sql
@@ -1,53 +1,50 @@
+-- Status Schedules
+ALTER TABLE IF EXISTS status_schedules 
+    DROP CONSTRAINT IF EXISTS fk_status_schedules_evse_id;
+
+DROP TABLE IF EXISTS status_schedules;
+
 -- Evse Parking Restrictions
-ALTER TABLE evse_parking_restrictions 
+ALTER TABLE IF EXISTS evse_parking_restrictions 
     DROP CONSTRAINT IF EXISTS fk_evse_parking_restrictions_parking_restriction_id;
 
-ALTER TABLE evse_parking_restrictions 
+ALTER TABLE IF EXISTS evse_parking_restrictions 
     DROP CONSTRAINT IF EXISTS fk_evse_parking_restrictions_evse_id;
 
 DROP TABLE IF EXISTS evse_parking_restrictions;
 
 -- Evse Capabilities
-ALTER TABLE evse_capabilities 
+ALTER TABLE IF EXISTS evse_capabilities 
     DROP CONSTRAINT IF EXISTS fk_evse_capabilities_capability_id;
 
-ALTER TABLE evse_capabilities 
+ALTER TABLE IF EXISTS evse_capabilities 
     DROP CONSTRAINT IF EXISTS fk_evse_capabilities_evse_id;
 
 DROP TABLE IF EXISTS evse_capabilities;
 
 -- Evse Images
-ALTER TABLE evse_images 
+ALTER TABLE IF EXISTS evse_images 
     DROP CONSTRAINT IF EXISTS fk_evse_images_image_id;
 
-ALTER TABLE evse_images 
+ALTER TABLE IF EXISTS evse_images 
     DROP CONSTRAINT IF EXISTS fk_evse_images_location_id;
 
 DROP TABLE IF EXISTS evse_images;
 
 -- Evse Directions
-ALTER TABLE evse_directions 
+ALTER TABLE IF EXISTS evse_directions 
     DROP CONSTRAINT IF EXISTS fk_evse_directions_display_text_id;
 
-ALTER TABLE evse_directions 
+ALTER TABLE IF EXISTS evse_directions 
     DROP CONSTRAINT IF EXISTS fk_evse_directions_evse_id;
 
 DROP TABLE IF EXISTS evse_directions;
 
--- EVSEs
-ALTER TABLE evses 
-    DROP CONSTRAINT IF EXISTS fk_evses_location_id;
-
-DROP INDEX IF EXISTS idx_evses_uid;
-
-DROP TABLE IF EXISTS evses;
-
-DROP TABLE IF EXISTS parking_restrictions;
-
-ALTER TABLE connectors 
+-- Connectors
+ALTER TABLE IF EXISTS connectors 
     DROP CONSTRAINT IF EXISTS fk_connectors_evse_id;
 
-ALTER TABLE connectors 
+ALTER TABLE IF EXISTS connectors 
     DROP CONSTRAINT IF EXISTS uq_connectors_evse_uid;
 
 DROP INDEX IF EXISTS idx_connectors_uid;
@@ -60,8 +57,16 @@ DROP TYPE IF EXISTS connector_format;
 
 DROP TYPE IF EXISTS connector_type;
 
-DROP TABLE IF EXISTS capabilities;
+-- EVSEs
+ALTER TABLE IF EXISTS evses 
+    DROP CONSTRAINT IF EXISTS fk_evses_location_id;
 
-DROP TABLE IF EXISTS status_schedules;
+DROP INDEX IF EXISTS idx_evses_uid;
+
+DROP TABLE IF EXISTS evses;
+
+DROP TABLE IF EXISTS parking_restrictions;
+
+DROP TABLE IF EXISTS capabilities;
 
 DROP TYPE IF EXISTS evse_status;
--- db/migrations/20211115113050_create_versions_table.down.sql
+++ db/migrations/20211115113050_create_versions_table.down.sql
@@ -1,11 +1,11 @@
 -- Version Endpoints
-ALTER TABLE version_endpoints 
+ALTER TABLE IF EXISTS version_endpoints
     DROP CONSTRAINT IF EXISTS fk_version_endpoints_version_id;
 
 DROP TABLE IF EXISTS version_endpoints;
 
 -- Versions
-ALTER TABLE versions 
+ALTER TABLE IF EXISTS versions 
     DROP CONSTRAINT IF EXISTS fk_versions_credentials_id;
 
 DROP TABLE IF EXISTS versions;
