4015179ae830ae6930db7be75cbcb2a095f1bc6a
Ross Savage <ross.savage@agrista.com>
2021-11-23 15:00:00 +0100

    Add email_subscriptions code column and update query

--- db/repository.go
+++ db/repository.go
@@ -1,24 +1,10 @@
 package db
 
 import (
-	"context"
 	"database/sql"
 
 	_ "github.com/lib/pq"
 )
-
-type Repository interface {
-	//User
-	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
-	DeleteUser(ctx context.Context, id int64) error
-	GetUser(ctx context.Context, id int64) (User, error)
-	ListUsers(ctx context.Context) ([]User, error)
-	UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error)
-
-	// Node
-	CreateNode(ctx context.Context, arg CreateNodeParams) (Node, error)
-	GetNode(ctx context.Context, id int64) (Node, error)
-}
 
 type RepositoryService struct {
 	*Queries
