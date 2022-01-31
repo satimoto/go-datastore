1f6a0f616ce717ba0442a44e5876de25d2a3d5ef
Ross Savage <ross.savage@agrista.com>
2022-01-31 13:48:22 +0100

    Add columns status and settled_msat to channel request table, add channel request HTLCs table

--- db/migrations/20220106134841_create_channel_requests_table.down.sql
+++ db/migrations/20220106134841_create_channel_requests_table.down.sql
@@ -1,4 +1,13 @@
+-- Channel Requests
 ALTER TABLE IF EXISTS channel_requests 
   DROP CONSTRAINT IF EXISTS uq_channel_requests_payment_hash;
 
 DROP TABLE IF EXISTS channel_requests;
+
+DROP TYPE IF EXISTS channel_request_status;
+
+-- Channel Request HTLCs
+ALTER TABLE IF EXISTS channel_request_htlcs 
+    DROP CONSTRAINT IF EXISTS fk_channel_request_htlcs_channel_request_id;
+
+DROP TABLE IF EXISTS channel_request_htlcs;
--- db/migrations/20220106134841_create_channel_requests_table.up.sql
+++ db/migrations/20220106134841_create_channel_requests_table.up.sql
@@ -1,13 +1,38 @@
+-- Channel Requests
+CREATE TYPE channel_request_status AS ENUM (
+    'REQUESTED', 
+    'AWAITING_PAYMENTS',
+    'OPENING_CHANNEL',
+    'COMPLETED',
+    'FAILED'
+);
 
 CREATE TABLE IF NOT EXISTS channel_requests (
-    id BIGSERIAL PRIMARY KEY,
-    pubkey BYTEA NOT NULL,
-    payment_hash BYTEA NOT NULL,
-    payment_addr BYTEA NOT NULL,
-    amount_msat BIGINT NOT NULL, 
+    id            BIGSERIAL PRIMARY KEY,
+    status        channel_request_status NOT NULL,
+    pubkey        BYTEA NOT NULL,
+    payment_hash  BYTEA NOT NULL,
+    payment_addr  BYTEA NOT NULL,
+    amount_msat   BIGINT NOT NULL, 
+    settled_msat  BIGINT NOT NULL, 
     funding_tx_id BYTEA,
-    output_index BIGINT
+    output_index  BIGINT
 );
 
 ALTER TABLE channel_requests 
     ADD CONSTRAINT uq_channel_requests_payment_hash UNIQUE (payment_hash);
+
+-- Channel Request HTLCs
+CREATE TABLE IF NOT EXISTS channel_request_htlcs (
+    id                 BIGSERIAL PRIMARY KEY,
+    channel_request_id BIGINT NOT NULL,
+    chan_id            BIGINT NOT NULL,
+    htlc_id            BIGINT NOT NULL,
+    is_settled         BOOLEAN NOT NULL
+);
+
+ALTER TABLE channel_request_htlcs 
+    ADD CONSTRAINT fk_channel_request_htlcs_channel_request_id
+    FOREIGN KEY (channel_request_id) 
+    REFERENCES channel_requests(id) 
+    ON DELETE CASCADE;
