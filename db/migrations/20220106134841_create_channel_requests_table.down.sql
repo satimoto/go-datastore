5faa54d081f9ea8b57f3a8428d32e688a1138c5e
Ross Savage <ross.savage@agrista.com>
2022-02-25 09:20:01 +0100

    Fix migration

--- db/migrations/20220106134841_create_channel_requests_table.down.sql
+++ db/migrations/20220106134841_create_channel_requests_table.down.sql
@@ -1,3 +1,9 @@
+-- Channel Request HTLCs
+ALTER TABLE IF EXISTS channel_request_htlcs 
+    DROP CONSTRAINT IF EXISTS fk_channel_request_htlcs_channel_request_id;
+
+DROP TABLE IF EXISTS channel_request_htlcs;
+
 -- Channel Requests
 ALTER TABLE IF EXISTS channel_requests 
   DROP CONSTRAINT IF EXISTS uq_channel_requests_payment_hash;
@@ -6,8 +12,3 @@ DROP TABLE IF EXISTS channel_requests;
 
 DROP TYPE IF EXISTS channel_request_status;
 
--- Channel Request HTLCs
-ALTER TABLE IF EXISTS channel_request_htlcs 
-    DROP CONSTRAINT IF EXISTS fk_channel_request_htlcs_channel_request_id;
-
-DROP TABLE IF EXISTS channel_request_htlcs;
--- db/migrations/20220223081916_alter_user_table.down.sql
+++ db/migrations/20220223081916_alter_user_table.down.sql
@@ -1,14 +1,22 @@
 -- Users
-ALTER TABLE users DROP CONSTRAINT uq_users_node_pubkey;
+ALTER TABLE IF EXISTS users 
+    DROP CONSTRAINT IF EXISTS uq_users_node_pubkey;
 
-ALTER TABLE users ADD node_address TEXT;
-ALTER TABLE users ALTER node_address SET DEFAULT '';
-ALTER TABLE users ALTER node_address SET NOT NULL;
+ALTER TABLE IF EXISTS users 
+    ADD node_address TEXT;
+ALTER TABLE IF EXISTS users 
+    ALTER node_address SET DEFAULT '';
+ALTER TABLE IF EXISTS users 
+    ALTER node_address SET NOT NULL;
 
-ALTER TABLE users RENAME linking_pubkey TO linking_key;
-ALTER TABLE users RENAME node_pubkey TO node_key;
+ALTER TABLE IF EXISTS users 
+    RENAME COLUMN IF EXISTS linking_pubkey TO linking_key;
+ALTER TABLE IF EXISTS users 
+    RENAME COLUMN IF EXISTS node_pubkey TO node_key;
 
-ALTER TABLE users ADD CONSTRAINT uq_users_node_key UNIQUE (node_key);
+ALTER TABLE IF EXISTS users 
+    ADD CONSTRAINT IF NOT EXISTS uq_users_node_key UNIQUE (node_key);
 
 -- Authentications
-ALTER TABLE authentications RENAME linking_key TO linking_pubkey;
+ALTER TABLE IF EXISTS authentications 
+    RENAME COLUMN IF EXISTS linking_pubkey TO linking_key;
