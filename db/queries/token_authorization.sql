80bbcf7335895930c655990d512b17d9e2f98cf7
Ross Savage <ross.savage@agrista.com>
2022-04-05 09:52:33 +0200

    Add GetTokenAuthorizationByAuthorizationID query

--- db/queries/token_authorization.sql
+++ db/queries/token_authorization.sql
@@ -7,3 +7,7 @@ INSERT INTO token_authorizations (
     location_id
   ) VALUES ($1, $2, $3, $4, $5)
   RETURNING *;
+
+-- name: GetTokenAuthorizationByAuthorizationID :one
+SELECT * FROM token_authorizations
+  WHERE authorization_id = $1;
