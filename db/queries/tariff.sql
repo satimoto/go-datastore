9787691bc12c6120d827fd8e3f012c7a9b8df46c
Ross Savage <ross.savage@agrista.com>
2022-04-05 16:51:57 +0200

    Add TariffRestrictionID to Tariff queries

--- db/queries/tariff.sql
+++ db/queries/tariff.sql
@@ -7,8 +7,9 @@ INSERT INTO tariffs (
     currency, 
     tariff_alt_url, 
     energy_mix_id, 
+    tariff_restriction_id,
     last_updated
-  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
   RETURNING *;
 
 -- name: DeleteTariffByUid :exec
@@ -31,7 +32,8 @@ UPDATE tariffs SET (
     currency, 
     tariff_alt_url,
     energy_mix_id, 
+    tariff_restriction_id,
     last_updated
-  ) = ($2, $3, $4, $5, $6, $7)
+  ) = ($2, $3, $4, $5, $6, $7, $8)
   WHERE uid = $1 AND cdr_id IS NULL
   RETURNING *;
