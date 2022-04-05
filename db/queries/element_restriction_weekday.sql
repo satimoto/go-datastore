1f729dc61442395b37010766ed45f951a23b74fb
Ross Savage <ross.savage@agrista.com>
2022-04-05 15:14:26 +0200

    Fix ElementRestrictionWeekday queries

--- db/queries/element_restriction_weekday.sql
+++ db/queries/element_restriction_weekday.sql
@@ -1,13 +1,13 @@
--- name: SetRestrictionWeekday :exec
+-- name: SetElementRestrictionWeekday :exec
 INSERT INTO element_restriction_weekdays 
     (element_restriction_id, weekday_id)
   VALUES ($1, $2);
 
--- name: UnsetRestrictionWeekdays :exec
+-- name: UnsetElementRestrictionWeekdays :exec
 DELETE FROM element_restriction_weekdays rw
   WHERE rw.element_restriction_id == $1;
 
--- name: ListRestrictionWeekdays :many
+-- name: ListElementRestrictionWeekdays :many
 SELECT w.* FROM weekdays w
   INNER JOIN element_restriction_weekdays rw ON rw.weekday_id == w.id
   WHERE rw.element_restriction_id == $1
