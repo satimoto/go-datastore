6ace5b6767b14f024ab39ac147900174128294fe
Ross Savage <ross.savage@agrista.com>
2022-04-15 15:49:46 +0200

    Fix GetLocationByLastUpdated types

--- db/location.sql.go
+++ db/location.sql.go
@@ -213,13 +213,23 @@ func (q *Queries) GetLocation(ctx context.Context, id int64) (Location, error) {
 	return i, err
 }
 
-const getLocationByUid = `-- name: GetLocationByUid :one
+const getLocationByLastUpdated = `-- name: GetLocationByLastUpdated :one
 SELECT id, uid, credential_id, country_code, party_id, type, name, address, city, postal_code, country, geom, geo_location_id, available_evses, total_evses, is_remote_capable, is_rfid_capable, operator_id, suboperator_id, owner_id, time_zone, opening_time_id, charging_when_closed, energy_mix_id, last_updated FROM locations
-  WHERE uid = $1
+  WHERE ($1::BIGINT = -1 OR $1::BIGINT = credental_id) AND
+    ($2::TEXT = '' OR $2::TEXT = country_code) AND
+    ($3::TEXT = '' OR $3::TEXT = party_id)
+  ORDER BY last_updated DESC
+  LIMIT 1
 `
 
-func (q *Queries) GetLocationByUid(ctx context.Context, uid string) (Location, error) {
-	row := q.db.QueryRowContext(ctx, getLocationByUid, uid)
+type GetLocationByLastUpdatedParams struct {
+	CredentalID int64  `db:"credental_id" json:"credentalID"`
+	CountryCode string `db:"country_code" json:"countryCode"`
+	PartyID     string `db:"party_id" json:"partyID"`
+}
+
+func (q *Queries) GetLocationByLastUpdated(ctx context.Context, arg GetLocationByLastUpdatedParams) (Location, error) {
+	row := q.db.QueryRowContext(ctx, getLocationByLastUpdated, arg.CredentalID, arg.CountryCode, arg.PartyID)
 	var i Location
 	err := row.Scan(
 		&i.ID,
@@ -251,23 +261,13 @@ func (q *Queries) GetLocationByUid(ctx context.Context, uid string) (Location, e
 	return i, err
 }
 
-const getLocationLastUpdated = `-- name: GetLocationLastUpdated :one
+const getLocationByUid = `-- name: GetLocationByUid :one
 SELECT id, uid, credential_id, country_code, party_id, type, name, address, city, postal_code, country, geom, geo_location_id, available_evses, total_evses, is_remote_capable, is_rfid_capable, operator_id, suboperator_id, owner_id, time_zone, opening_time_id, charging_when_closed, energy_mix_id, last_updated FROM locations
-  WHERE ($1::BIGINT = -1 OR $1::BIGINT = credental_id) AND
-    ($2::BIGINT = '' OR $2::BIGINT = country_code) AND
-    ($3::BIGINT = '' OR $3::BIGINT = party_id)
-  ORDER BY last_updated DESC
-  LIMIT 1
+  WHERE uid = $1
 `
 
-type GetLocationLastUpdatedParams struct {
-	CredentalID int64 `db:"credental_id" json:"credentalID"`
-	CountryCode int64 `db:"country_code" json:"countryCode"`
-	PartyID     int64 `db:"party_id" json:"partyID"`
-}
-
-func (q *Queries) GetLocationLastUpdated(ctx context.Context, arg GetLocationLastUpdatedParams) (Location, error) {
-	row := q.db.QueryRowContext(ctx, getLocationLastUpdated, arg.CredentalID, arg.CountryCode, arg.PartyID)
+func (q *Queries) GetLocationByUid(ctx context.Context, uid string) (Location, error) {
+	row := q.db.QueryRowContext(ctx, getLocationByUid, uid)
 	var i Location
 	err := row.Scan(
 		&i.ID,
