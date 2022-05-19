0ab5e14c62eff8378026c9b750096368913ec91e
Ross Savage <ross.savage@agrista.com>
2022-05-19 21:01:54 +0100

    Remove ImageResolver and NodeResolver

---
+++ pkg/node/mocks/mock_repository.go
@@ -0,0 +1,10 @@
+package mocks
+
+import (
+	mocks "github.com/satimoto/go-datastore/pkg/db/mocks"
+	"github.com/satimoto/go-datastore/pkg/node"
+)
+
+func NewResolver(repositoryService *mocks.MockRepositoryService) node.NodeRepository {
+	return node.NodeRepository(repositoryService)
+}
