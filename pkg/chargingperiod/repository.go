caf4e6589e36df5bc1fb753f40c8afe7ebcca1e1
Ross Savage <ross.savage@agrista.com>
2022-05-21 20:27:34 +0100

    FIx charging period interface

--- pkg/chargingperiod/repository.go
+++ pkg/chargingperiod/repository.go
@@ -2,23 +2,16 @@ package chargingperiod
 
 import (
 	"context"
+	"time"
 
 	"github.com/satimoto/go-datastore/pkg/db"
 )
 
 type ChargingPeriodRepository interface {
-	CreateCommandReservation(ctx context.Context, arg db.CreateCommandReservationParams) (db.CommandReservation, error)
-	CreateCommandStart(ctx context.Context, arg db.CreateCommandStartParams) (db.CommandStart, error)
-	CreateCommandStop(ctx context.Context, arg db.CreateCommandStopParams) (db.CommandStop, error)
-	CreateCommandUnlock(ctx context.Context, arg db.CreateCommandUnlockParams) (db.CommandUnlock, error)
-	GetCommandReservation(ctx context.Context, id int64) (db.CommandReservation, error)
-	GetCommandStart(ctx context.Context, id int64) (db.CommandStart, error)
-	GetCommandStop(ctx context.Context, id int64) (db.CommandStop, error)
-	GetCommandUnlock(ctx context.Context, id int64) (db.CommandUnlock, error)
-	UpdateCommandReservation(ctx context.Context, arg db.UpdateCommandReservationParams) (db.CommandReservation, error)
-	UpdateCommandStart(ctx context.Context, arg db.UpdateCommandStartParams) (db.CommandStart, error)
-	UpdateCommandStop(ctx context.Context, arg db.UpdateCommandStopParams) (db.CommandStop, error)
-	UpdateCommandUnlock(ctx context.Context, arg db.UpdateCommandUnlockParams) (db.CommandUnlock, error)
+	CreateChargingPeriod(ctx context.Context, startDateTime time.Time) (db.ChargingPeriod, error)
+	CreateChargingPeriodDimension(ctx context.Context, arg db.CreateChargingPeriodDimensionParams) (db.ChargingPeriodDimension, error)
+	DeleteChargingPeriodDimensions(ctx context.Context, chargingPeriodID int64) error
+	ListChargingPeriodDimensions(ctx context.Context, chargingPeriodID int64) ([]db.ChargingPeriodDimension, error)
 }
 
 func NewRepository(repositoryService *db.RepositoryService) ChargingPeriodRepository {
