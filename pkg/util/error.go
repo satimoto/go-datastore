034755106d10ff816355e0829a51c112212eb430
Ross Savage <ross.savage@agrista.com>
2022-06-10 23:13:27 +0200

    Fix PanicOnError not logging err

--- pkg/util/error.go
+++ pkg/util/error.go
@@ -15,7 +15,7 @@ func LogOnError(code string, message string, err error) {
 
 func PanicOnError(code string, message string, err error) {
 	if err != nil {
-		log.Fatalf("%s: %s", code, message)
+		log.Printf("%s: %s", code, message)
 		log.Fatalf("%s: %v", code, err)
 	}
 }
