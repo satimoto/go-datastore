2b4bf339dc759323f99357658076d5aac2a3e0e2
Ross Savage <ross.savage@agrista.com>
2022-06-16 15:03:31 +0200

    Fix GetVersionEndpointByIdentity query

--- internal/queries/version_endpoint.sql
+++ internal/queries/version_endpoint.sql
@@ -16,7 +16,7 @@ SELECT * FROM version_endpoints
 
 -- name: GetVersionEndpointByIdentity :one
 SELECT ve.* FROM version_endpoints ve
-  INNER JOIN credentials c ON vc.version_id = c.version_id
+  INNER JOIN credentials c ON ve.version_id = c.version_id
   WHERE ve.identifier = $1 AND c.country_code = $2 AND c.party_id = $3;
 
 -- name: ListVersionEndpoints :many
