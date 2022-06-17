3fa92f2e9ee92197f2a73f5f228f6d0638a4f73d
Ross Savage <ross.savage@agrista.com>
2022-06-17 10:13:46 +0200

    Add DisplayText queries to repository and mocks

--- pkg/displaytext/repository.go
+++ pkg/displaytext/repository.go
@@ -8,6 +8,8 @@ import (
 
 type DisplayTextRepository interface {
 	CreateDisplayText(ctx context.Context, arg db.CreateDisplayTextParams) (db.DisplayText, error)
+	DeleteDisplayText(ctx context.Context, id int64) error
+	GetDisplayText(ctx context.Context, id int64) (db.DisplayText, error)
 }
 
 func NewRepository(repositoryService *db.RepositoryService) DisplayTextRepository {
