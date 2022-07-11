8bc89eb3a7368b8835f1f1c819f178bf338574d4
Ross Savage <ross.savage@agrista.com>
2022-07-11 22:33:00 +0200

    Add UpdateRoutingEvent query and migrations

--- pkg/routingevent/repository.go
+++ pkg/routingevent/repository.go
@@ -8,6 +8,7 @@ import (
 
 type RoutingEventRepository interface {
 	CreateRoutingEvent(ctx context.Context, arg db.CreateRoutingEventParams) (db.RoutingEvent, error)
+	UpdateRoutingEvent(ctx context.Context, arg db.UpdateRoutingEventParams) (db.RoutingEvent, error)
 }
 
 func NewRepository(repositoryService *db.RepositoryService) RoutingEventRepository {
