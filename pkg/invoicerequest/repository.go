675d9b6adb96807b981dea5f4667e5da08836141
Ross Savage <ross.savage@agrista.com>
2022-09-19 10:05:23 +0200

    Fix return type of GetInvoiceRequest

--- pkg/invoicerequest/repository.go
+++ pkg/invoicerequest/repository.go
@@ -9,6 +9,7 @@ import (
 type InvoiceRequestRepository interface {
 	CreateInvoiceRequest(ctx context.Context, arg db.CreateInvoiceRequestParams) (db.InvoiceRequest, error)
 	DeleteInvoiceRequest(ctx context.Context, id int64) error
+	GetInvoiceRequest(ctx context.Context, id int64) (db.InvoiceRequest, error)
 	GetUnsettledInvoiceRequest(ctx context.Context, arg db.GetUnsettledInvoiceRequestParams) (db.InvoiceRequest, error)
 	ListUnsettledInvoiceRequests(ctx context.Context, userID int64) ([]db.InvoiceRequest, error)
 	UpdateInvoiceRequest(ctx context.Context, arg db.UpdateInvoiceRequestParams) (db.InvoiceRequest, error)
