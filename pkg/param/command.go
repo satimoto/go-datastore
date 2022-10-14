9beb77989796f0254ed3affeff8814577ab6cdd7
Ross Savage <ross.savage@agrista.com>
2022-10-14 16:38:53 +0200

    Set LastUpdated in update params

--- pkg/param/command.go
+++ pkg/param/command.go
@@ -4,30 +4,34 @@ import "github.com/satimoto/go-datastore/pkg/db"
 
 func NewUpdateCommandReservationParams(command db.CommandReservation) db.UpdateCommandReservationParams {
 	return db.UpdateCommandReservationParams{
-		ID:         command.ID,
-		Status:     command.Status,
-		ExpiryDate: command.ExpiryDate,
-		EvseUid:    command.EvseUid,
+		ID:          command.ID,
+		Status:      command.Status,
+		ExpiryDate:  command.ExpiryDate,
+		EvseUid:     command.EvseUid,
+		LastUpdated: command.LastUpdated,
 	}
 }
 
 func NewUpdateCommandStartParams(command db.CommandStart) db.UpdateCommandStartParams {
 	return db.UpdateCommandStartParams{
-		ID:     command.ID,
-		Status: command.Status,
+		ID:          command.ID,
+		Status:      command.Status,
+		LastUpdated: command.LastUpdated,
 	}
 }
 
 func NewUpdateCommandStopParams(command db.CommandStop) db.UpdateCommandStopParams {
 	return db.UpdateCommandStopParams{
-		ID:     command.ID,
-		Status: command.Status,
+		ID:          command.ID,
+		Status:      command.Status,
+		LastUpdated: command.LastUpdated,
 	}
 }
 
 func NewUpdateCommandUnlockParams(command db.CommandUnlock) db.UpdateCommandUnlockParams {
 	return db.UpdateCommandUnlockParams{
-		ID:     command.ID,
-		Status: command.Status,
+		ID:          command.ID,
+		Status:      command.Status,
+		LastUpdated: command.LastUpdated,
 	}
 }
