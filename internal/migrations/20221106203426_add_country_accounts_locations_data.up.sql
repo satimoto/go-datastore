a586c6e8aaa90b62f661dcfeb755fe48661df602
Ross Savage <ross.savage@agrista.com>
2022-11-07 19:50:04 +0100

    Add country account locations data

---
+++ internal/migrations/20221106203426_add_country_accounts_locations_data.up.sql
@@ -0,0 +1,61 @@
+-- Country accounts
+INSERT INTO country_accounts (country, tax_percent) VALUES 
+    ('ALB', 20),
+    ('BIH', 17),
+    ('CHE', 8),
+    ('DMA', 15),
+    ('GLP', 8.5),
+    ('IRL', 23),
+    ('LIE', 7.7),
+    ('MKD', 18),
+    ('MNE', 21),
+    ('MTQ', 8.5),
+    ('NOR', 25),
+    ('SRB', 20);
+
+UPDATE country_accounts SET longitude = 19.9708, latitude = 41.1350, zoom = 7.62 WHERE country = 'ALB';
+UPDATE country_accounts SET longitude = 13.3834, latitude = 47.7740, zoom = 6.77 WHERE country = 'AUT';
+UPDATE country_accounts SET longitude = 4.5152, latitude = 50.5642, zoom = 7.72 WHERE country = 'BEL';
+UPDATE country_accounts SET longitude = 25.5008, latitude = 42.6360, zoom = 7.06 WHERE country = 'BGR';
+UPDATE country_accounts SET longitude = 17.6942, latitude = 44.0175, zoom = 7.71 WHERE country = 'BIH';
+UPDATE country_accounts SET longitude = 8.2493, latitude = 46.9084, zoom = 7.59 WHERE country = 'CHE';
+UPDATE country_accounts SET longitude = -70.6474, latitude = -35.8289, zoom = 4.23 WHERE country = 'CHL';
+UPDATE country_accounts SET longitude = -74.0342, latitude = 4.3242, zoom = 5.67 WHERE country = 'COL';
+UPDATE country_accounts SET longitude = 15.5197, latitude = 49.8237, zoom = 7.03 WHERE country = 'CZE';
+UPDATE country_accounts SET longitude = 9.9284, latitude = 51.3055, zoom = 6.19 WHERE country = 'DEU';
+UPDATE country_accounts SET longitude = -61.3586, latitude = 15.4255, zoom = 10.89 WHERE country = 'DMA';
+UPDATE country_accounts SET longitude = 10.3124, latitude = 56.2122, zoom = 7.28 WHERE country = 'DNK';
+UPDATE country_accounts SET longitude = -2.4335, latitude = 40.1671, zoom = 5.92 WHERE country = 'ESP';
+UPDATE country_accounts SET longitude = 24.9938, latitude = 58.7382, zoom = 7.11 WHERE country = 'EST';
+UPDATE country_accounts SET longitude = 25.2534, latitude = 65.0556, zoom = 5.25 WHERE country = 'FIN';
+UPDATE country_accounts SET longitude = 2.4548, latitude = 46.6082, zoom = 5.87 WHERE country = 'FRA';
+UPDATE country_accounts SET longitude = -3.6452, latitude = 55.6785, zoom = 5.52 WHERE country = 'GBR';
+UPDATE country_accounts SET longitude = -61.3995, latitude = 16.2197, zoom = 10.06 WHERE country = 'GLP';
+UPDATE country_accounts SET longitude = 23.9138, latitude = 38.2706, zoom = 6.68 WHERE country = 'GRC';
+UPDATE country_accounts SET longitude = 16.5263, latitude = 44.5723, zoom = 7.15 WHERE country = 'HRV';
+UPDATE country_accounts SET longitude = 19.5152, latitude = 47.3331, zoom = 7.00 WHERE country = 'HUN';
+UPDATE country_accounts SET longitude = -4.5465, latitude = 54.2355, zoom = 10.39 WHERE country = 'IMN';
+UPDATE country_accounts SET longitude = -8.0639, latitude = 53.4781, zoom = 7.03 WHERE country = 'IRL';
+UPDATE country_accounts SET longitude = -18.779, latitude = 65.1463, zoom = 6.24 WHERE country = 'ISL';
+UPDATE country_accounts SET longitude = 12.5389, latitude = 42.1121, zoom = 5.95 WHERE country = 'ITA';
+UPDATE country_accounts SET longitude = 127.9046, latitude = 35.9429, zoom = 7.01 WHERE country = 'KOR';
+UPDATE country_accounts SET longitude = 9.5505, latitude = 47.1589, zoom = 11.36 WHERE country = 'LIE';
+UPDATE country_accounts SET longitude = 23.7073, latitude = 55.3415, zoom = 7.11 WHERE country = 'LTU';
+UPDATE country_accounts SET longitude = 6.0947, latitude = 49.8172, zoom = 9.57 WHERE country = 'LUX';
+UPDATE country_accounts SET longitude = 24.5561, latitude = 57.0417, zoom = 6.91 WHERE country = 'LVA';
+UPDATE country_accounts SET longitude = -7.0711, latitude = 32.1637, zoom = 6.10 WHERE country = 'MAR';
+UPDATE country_accounts SET longitude = 21.7481, latitude = 41.6061, zoom = 8.37 WHERE country = 'MKD';
+UPDATE country_accounts SET longitude = 14.3836, latitude = 35.9400, zoom = 10.92 WHERE country = 'MLT';
+UPDATE country_accounts SET longitude = 19.3236, latitude = 42.6969, zoom = 8.57 WHERE country = 'MNE';
+UPDATE country_accounts SET longitude = -61.0115, latitude = 14.6416, zoom = 10.79 WHERE country = 'MTQ';
+UPDATE country_accounts SET longitude = 5.4662, latitude = 52.1962, zoom = 7.57 WHERE country = 'NLD';
+UPDATE country_accounts SET longitude = 14.2844, latitude = 64.9976, zoom = 4.84 WHERE country = 'NOR';
+UPDATE country_accounts SET longitude = -74.5947, latitude = -9.1322, zoom = 5.58 WHERE country = 'PER';
+UPDATE country_accounts SET longitude = 19.1801, latitude = 52.1817, zoom = 6.40 WHERE country = 'POL';
+UPDATE country_accounts SET longitude = -8.0980, latitude = 39.6341, zoom = 7.06 WHERE country = 'PRT';
+UPDATE country_accounts SET longitude = 25.1236, latitude = 46.1440, zoom = 6.48 WHERE country = 'ROU';
+UPDATE country_accounts SET longitude = 20.6744, latitude = 44.2389, zoom = 7.30 WHERE country = 'SRB';
+UPDATE country_accounts SET longitude = 19.7579, latitude = 48.7545, zoom = 7.22 WHERE country = 'SVK';
+UPDATE country_accounts SET longitude = 14.9946, latitude = 46.0477, zoom = 8.05 WHERE country = 'SVN';
+UPDATE country_accounts SET longitude = 15.8452, latitude = 62.3552, zoom = 4.93 WHERE country = 'SWE';
+UPDATE country_accounts SET longitude = 101.2937, latitude = 13.2713, zoom = 5.88 WHERE country = 'THA';
