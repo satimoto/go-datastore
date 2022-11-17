bec42e9d39fd0c0049ce91974ee5e9b90bbba1fe
Ross Savage <ross.savage@agrista.com>
2022-11-17 23:55:01 +0100

    Update data sql processing

---
+++ data/connector_check.sql
@@ -0,0 +1,4 @@
+SELECT l.country, l.uid, c.* FROM locations l
+  LEFT JOIN evses e ON e.location_id = l.id
+  LEFT JOIN connectors c ON c.evse_id = e.id
+  WHERE country_code = 'NL' AND party_id = 'CPI'
