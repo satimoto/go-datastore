1e46964898593624510017c5b3ebfef75e69f511
Ross Savage <ross.savage@agrista.com>
2022-12-03 22:47:18 +0100

    Add IsRemoteCapable and isRfidCapable to location filtering

--- data/connector_check.sql
+++ data/connector_check.sql
@@ -13,3 +13,11 @@ SELECT l.country, COUNT(l.country) AS cc FROM locations l
   WHERE l.publish
   GROUP BY country
   ORDER BY cc DESC;
+
+-- Added in the last week
+SELECT l.country, l.uid, l.country_code, l.party_id, l.publish, l.added_date, c.* FROM locations l
+  LEFT JOIN evses e ON e.location_id = l.id
+  LEFT JOIN connectors c ON c.evse_id = e.id
+  WHERE l.added_date > NOW() - interval '1 week' AND c.tariff_id is not null AND l.publish = false;
+
+
